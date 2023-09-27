63f92ce670d9a72d508a3529dc9fd90d008c385b
Tanbir Ahmed <145252315+tdottahmed@users.noreply.github.com>
2023-09-27 16:27:21 +0600

    1st deploy

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "e35528f531mshaad9e85f2ab9a33p1c6105jsnf23fefc503fa", // enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "13f841a46a9906830f6194b6404be93d"; // enter your key from openweather API
